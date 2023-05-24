3269aa4385db3ed3e027ba203caff44f3170e853
Scott-S34 <115486178+Scott-S34@users.noreply.github.com>
2023-05-24 16:00:27 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 //---------------------------------------- DEFINE ROUTES ---------------------------------------- //
 //define a route for the home page
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Home'});
+    res.render('index', {title: 'Home', 
+    stations: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    timePerStation: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+      16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+  });
 });
 
 //define a route for the room1 page
@@ -27,8 +31,10 @@ app.get('/room1', (req, res) => {
 
 //define a route for the room2 page
 app.get('/room2', (req, res) => {
-  res.render('room2', {title: 'room2', capacity: 64});
+  res.render('room2', {title: 'room2', capacity: 64  });
 });
+
+
 
 app.listen(port, () => {
   console.log('Server running at http:// ' + port)
