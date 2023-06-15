6c3e38efff5e7f05109e8aa3ed95758cf426d8f8
Scott-S34 <115486178+Scott-S34@users.noreply.github.com>
2023-06-15 15:02:04 +0100

    Add files via upload

--- public/scripts/bell.js
+++ public/scripts/bell.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 
     $("#test").click(function(){
-        var bell = new Audio("audio/notification-bell.wav");
+        var bell = new Audio("audio/osce-recordings/one.wav");
     bell.play();});
   });
 
-function playAudio(){
-    var bell = new Audio("audio/notification-bell.wav");
-    bell.play();
-}
+// function playAudio(){
+//     var bell = new Audio("audio/school-bell.wav");
+//     bell.play();
+// }
 
-//document.getElementById("test").onclick(playAudio());
+// document.getElementById("test").onclick(playAudio());
+
+
